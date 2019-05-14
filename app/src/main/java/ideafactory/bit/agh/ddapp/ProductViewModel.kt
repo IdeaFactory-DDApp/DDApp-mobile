f8917984596caa25e3560a6551517c197f4369f4
konradkurzydym <konrad.kurzydym@gmail.com>
2019-05-14 11:23:31 +0200

    Created viewmodel file

--- app/src/main/java/ideafactory/bit/agh/ddapp/ProductViewModel.kt
+++ app/src/main/java/ideafactory/bit/agh/ddapp/ProductViewModel.kt
@@ -2,6 +2,6 @@ package ideafactory.bit.agh.ddapp
 
 import android.arch.lifecycle.ViewModel;
 
-class ProductViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProductViewModel() : ViewModel() {
+
 }
