18b2e8387451bb76e05e5a6a165d5348ecd42db0
sgrzegorz <grzegorzsroka2@gmail.com>
2019-05-18 22:03:31 +0200

    next step1

--- app/src/main/java/ideafactory/bit/agh/ddapp/KnowledgeActivity.kt
+++ app/src/main/java/ideafactory/bit/agh/ddapp/KnowledgeActivity.kt
@@ -3,6 +3,7 @@ package ideafactory.bit.agh.ddapp
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_knowledge.*
 
@@ -16,6 +17,12 @@ class KnowledgeActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        var name : String = "ola ma psa"
+        val textView: TextView = findViewById(R.id.information_view) as TextView
+        textView.setOnClickListener {
+            textView.text = "ola"
         }
     }
 
