a3523818d1929e0f00c15367ade3afb3aa68c96f
Aleksanderprofic <aleprofic@gmail.com>
2019-05-26 22:14:54 +0200

    Created three menu entries with fragments

---
+++ app/src/main/java/ideafactory/bit/agh/ddapp/ProductsFragment.kt
@@ -0,0 +1,19 @@
+package ideafactory.bit.agh.ddapp
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class ProductsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_products, container, false)
+    }
+
+
+}
