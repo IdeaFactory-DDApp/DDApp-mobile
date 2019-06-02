eb4586bd396e0a48a1aba44307a520de80d1aca6
konradkurzydym <konrad.kurzydym@gmail.com>
2019-06-02 22:59:42 +0200

    Styles and testable fragment
    Removing multiple occurences of the same properties by using style, fragment can now be tested by clicking FAB

--- app/src/main/java/ideafactory/bit/agh/ddapp/ProductFragment.kt
+++ app/src/main/java/ideafactory/bit/agh/ddapp/ProductFragment.kt
@@ -9,22 +9,16 @@ import android.view.ViewGroup
 
 
 class ProductFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ProductFragment()
-    }
-
     private lateinit var viewModel: ProductViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.product_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_product, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ProductViewModel::class.java)
-        // TODO: Use the ViewModel
+        //TODO: use the viewmodel
     }
-
 }
--- app/src/main/java/ideafactory/bit/agh/ddapp/ProductViewModel.kt
+++ app/src/main/java/ideafactory/bit/agh/ddapp/ProductViewModel.kt
@@ -3,5 +3,5 @@ package ideafactory.bit.agh.ddapp
 import android.arch.lifecycle.ViewModel;
 
 class ProductViewModel() : ViewModel() {
-
+    //TODO: create after database skeleton is done
 }
